82be4464f684adb5be1a0371c6e1ec7611431d4e
trunkey2007 <hoquanglam090@gmail.com>
2022-01-24 14:13:21 +0700

    Cookie added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Http;
+﻿using System.Net;
 
 namespace JSONEx
 {
@@ -18,19 +18,35 @@ namespace JSONEx
         }
         static async Task Main()
         {
-            var url = "http://localhost:5000/api/QuanLyNguoiDung/DangNhap";
-            var httpClient = new HttpClient(); 
+            var handler = new SocketsHttpHandler();
+            var cookies = new CookieContainer();
+
+            handler.UseCookies = true;
+            handler.CookieContainer = cookies;
+
+            var Server = "http://localhost:5000";
+            var url = $"{Server}/api/QuanLyNguoiDung/DangNhap";
+
+            var httpClient = new HttpClient(handler); 
             var httpResquestMessage = new HttpRequestMessage();
             httpResquestMessage.Method = HttpMethod.Post;
             httpResquestMessage.RequestUri = new Uri(url);
             httpResquestMessage.Headers.Add("User-Agent", "Mozilla/5.0");
+
             var parameters = new List<KeyValuePair<string,string>>();
             parameters.Add(new KeyValuePair<string,string>("taiKhoan", ""));
             parameters.Add(new KeyValuePair<string,string>("matKhau", ""));
+
             httpResquestMessage.Content = new FormUrlEncodedContent(parameters);
+
             var res = await httpClient.SendAsync(httpResquestMessage);
             var html = await res.Content.ReadAsStringAsync();
+
             Console.WriteLine(html);
+            
+            cookies.GetCookies(new Uri(url)).ToList().ForEach(cookie =>{
+                    Console.WriteLine(cookie);
+            });
         }
     }
 }
